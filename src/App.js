c39a17c734549979d36efd1cd659e96d23cfcc39
Muhammad Adnan Khalid <adnankhl@yahoo.com>
2024-04-01 12:36:21 +0800

    Update Buisness info

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>In the name of Allah.</h1>
+     <h1>FK Mobiles Parts.</h1>
+     <h2>Iphone and Samsung Mobile</h2>
+     <h2>Parts Original Battery, LCD</h2>
+     <h2>Pannels, Original Front Camera</h2>
+     <h2>Back Camera, Charging Ribbons Back GLass</h2>
+     <h2>And Much More Veriety.</h2>
+     <h2>Back Camera, Charging Ribbons Back GLass</h2>
+     <h1>FK Mobile Parts, LB-28</h1>
+     <h1>Star City Mall, Saddar, Abdullah Haroon Rd, Saddar</h1>
+     <h1>KArachi City, Sindh, Pakistan.</h1>
     </div>
   );
 }
